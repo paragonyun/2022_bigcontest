6c9ca0180c224c63a027eba7ffcc5c535b82b54a
paragonyun <paragonyun@naver.com>
2022-09-11 15:39:48 +0900

    Add EDAPreprocessing class for memory usage

--- src/eda.py
+++ src/eda.py
@@ -133,3 +133,59 @@ class EDA():
                     bbox_inches='tight', pad_inches=0)
 
         plt.show()
+
+'''
+This class is for the EFFICIENT USING of dataframe..! 
+Because our data sometimes takes 10G usage of memory... 
+I think it is very dangerous to our computers..!
+'''
+
+import time
+
+class EDAPreprocessing :
+    def __init__(self, df) :
+        self.df = df
+
+    def _check_times (original_fn) :
+        def wrapper_fn(*args, **kwargs) :
+            start = time.time()
+            result = original_fn(*args, **kwargs)
+            end = time.time()
+
+            print(f'{original_fn.__name__} 함수의 소요시간\n: {end-start:.2f}초')
+            
+            return result
+        return wrapper_fn
+
+
+    @_check_times
+    ## 효율적인 메모리 사용을 위한 다이어트
+    def diet_dataframe(self) :
+        cols = self.df.columns 
+
+        for i in cols :
+            if self.df[i].dtype =='int64' or self.df[i].dtype == 'float64' :
+                ## int와 float은 8, 16 으로 나타내면 나타낼 수 있는 수가 우리 데이터의 값을 담지 못함
+                ## 64보단 적은 32로 바꿈 (그래도 Memory Useage는 절반이나 줄어든다.)
+
+                if self.df[i].dtype =='int64' :
+                    self.df = self.df.astype({i : 'int32'})
+                    
+
+                else : 
+                    self.df = self.df.astype({i : 'float32'})
+
+            elif self.df[i].dtype == 'object' :
+                if 'time' in i or 'date' in i : ## 시간이나 날짜와 관련된 거면 그냥 넘김 (어떻게 처리할 것인지 논의 필요!)
+                    continue
+                
+                else :
+                    self.df = self.df.astype({ i : 'category'}) ## 범주가 너무 많지만 않다면 category가 더 효율적
+
+        return self.df  
+
+        '''
+        사용법 예시
+        trainer = EDAPreprocessing(df)
+        dieted_df = trainer.diet_dataframe()
+        '''
