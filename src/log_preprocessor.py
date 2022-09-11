76bf8e31e502222befd8f923a251016c97d0bbc6
Taeyeong <htyvv@naver.com>
2022-09-11 23:37:39 +0900

    Add preprocessor class

---
+++ src/log_preprocessor.py
@@ -0,0 +1,33 @@
+from .preprocessor import Preprocessor
+import pandas as pd
+
+class Log_Preprocessor(Preprocessor):
+    
+    def __init__(self, dataset: pd.DataFrame, prep_config: dict):
+        super().__init__(dataset, prep_config)
+        
+        
+    def __sample_prep_1(self, df: pd.DataFrame) -> pd.DataFrame:
+        # TODO
+        print('loan prep1')
+        return df
+    
+    
+    def __sample_prep_2(self, df: pd.DataFrame) -> pd.DataFrame:
+        # TODO
+        print('loan prep2')
+        return df
+    
+    
+    def __sample_prep_3(self, df: pd.DataFrame) -> pd.DataFrame:
+        # TODO
+        print('loan prep3')
+        return df
+    
+    
+    def _preprocess(self) -> pd.DataFrame:
+        prep_df = self.__sample_prep_1(self.raw_df)
+        prep_df = self.__sample_prep_2(prep_df)
+        prep_df = self.__sample_prep_3(prep_df)
+        # 여기서 index sort & reset 필수
+        return prep_df
