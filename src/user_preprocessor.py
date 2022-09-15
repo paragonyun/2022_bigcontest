9d63afdcfb7925b64b45d2620e1f298c9612551d
paragonyun <paragonyun@naver.com>
2022-09-15 12:30:36 +0900

    Revise Some Errors..!(datetime, inner functions)

--- src/user_preprocessor.py
+++ src/user_preprocessor.py
@@ -20,7 +20,7 @@ class User_Preprocessor(Preprocessor):
         self.ordinal_cols = ['birth_year','yearly_income']
 
         ## datetime type으로 바뀔 cols
-        self.time_cols = ['insert_time', 'company_enter_month']
+        self.time_cols = ['insert_time', 'company_enter_month', 'birth_year']
 
         ## 범주화될 cols
         self.categorical_cols = ['birth_year','yearly_income']
@@ -39,11 +39,16 @@ class User_Preprocessor(Preprocessor):
 
         ## datetime 대상이 될 cols를 돌면서
         for i in self.time_cols :
-            ## object 가 아니라 int나 뭐 그런 식이면
-            if output_df[i].dtype != 'object' :
-                ## dtype을 object로 바꾸고
-                output_df[i] = output_df[i].astype('object')
             
+            if i == 'birth_year' :
+
+                output_df[i] = pd.to_datetime(output_df[i], format='%Y', errors='ignore')
+                continue
+            
+            elif i == 'company_enter_month' :
+                output_df[i] = pd.to_datetime(output_df[i], format='%Y%m%d', errors='ignore')
+                continue
+
             ## 해당 column을 datetime type으로 바꿔줌
             output_df[i] = pd.to_datetime(output_df[i])
 
@@ -66,11 +71,11 @@ class User_Preprocessor(Preprocessor):
         print('카테고리화 시키는 중...')
         ## Birth 카테고리컬 화
         # 이미 생년월일은 datetime type으로 변환된 상태임
-        def ___calculate_age(self, x) :
+        def ___calculate_age(x) :
             this_yaer = datetime.now().year
             return this_yaer - x.year
 
-        def ___birth_category(self, x) :
+        def ___birth_category(x) :
             if 0 <= x < 10 :
                 return '0대'
             elif 10<= x <20 :
@@ -90,10 +95,10 @@ class User_Preprocessor(Preprocessor):
 
         output_df = df.copy()
         # 나이로 일단 변환
-        output_df['birth_year'] = output_df['birth_year'].apply(lambda x : ___calculate_age(x))
+        output_df['age'] = output_df['birth_year'].apply(lambda x : ___calculate_age(x))
 
         # 연령대로 변환
-        output_df['birth_year'] = output_df['birth_year'].apply(lambda x : ___birth_category(x))
+        output_df['age_cat'] = output_df['age'].apply(lambda x : ___birth_category(x))
 
         ## yearly_income 카테고리 화 (일단 내부 quntile 기준으로 했습니다.)
         # HACK 그러나 EDA를 하면 알 수 있듯이, 연소득이라는 것이 격차가 매우 커서 이 기준에 대해 다시 생각해보는 게 좋을 것같습니다.
@@ -101,7 +106,7 @@ class User_Preprocessor(Preprocessor):
         q50 = output_df['yearly_income'].quantile(.50)
         q75 = output_df['yearly_income'].quantile(.75)       
         
-        def ___income_category(self, x) :
+        def ___income_category(x) :
             if x < q25 :
                 return '1'
             elif q25 <= x <q50 :
