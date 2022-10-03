10512846d304b0d863287d7c4428e2641f3260fc
HJ <hanjun@MacBookAir.local>
2022-10-03 19:52:37 +0900

    mice

--- src/mice.py
+++ src/mice.py
@@ -8,20 +8,24 @@ class mice():
         self.df = df
         
     def mice_1(self):
-        mice_df = self.df[['credit_score','yearly_income','personal_rehabilitation_yn_0.0', 'personal_rehabilitation_yn_1.0',
+        mice_df = self.df[['credit_score','연령대', '소득분위','personal_rehabilitation_yn_0.0', 'personal_rehabilitation_yn_1.0',
        'personal_rehabilitation_yn_nan','existing_loan_cnt', 'existing_loan_amt']]
         
         imputer = IterativeImputer(imputation_order='ascending',max_iter=5,random_state=42)
         imputed_dataset_train = imputer.fit_transform(mice_df)
         train_mice = pd.DataFrame(imputed_dataset_train, columns=mice_df.columns)
         
-        a = list(train_mice['credit_score'].apply(lambda x : min(1000, int(x))))
-        b = list(train_mice['existing_loan_amt'])
+        credit_score = list(train_mice['credit_score'].apply(lambda x : min(1000, int(x))))
+        existing_loan_amt = list(train_mice['existing_loan_amt'])
         
-        self.df['mice_credit_score'] = a
-        self.df['mice_existing_loan_amt'] = b
-        self.df = self.df.drop(['credit_score','existing_loan_amt'], axis=1)
+        self.df['mice_credit_score'] = credit_score
+        self.df['mice_existing_loan_amt'] = existing_loan_amt
+        
+        self.df = self.df.drop(['credit_score','existing_loan_amt','income_per_credit','existing_loan_percent'], axis=1)
         
         self.df['loanamt_per_income'] = self.df['mice_existing_loan_amt'] / (1 + self.df['yearly_income'])
         self.df['loanamt_per_income'].fillna(0, inplace=True)
+        
+        self.df['income_per_credit'] = self.df['yearly_income'] / self.df['credit_score']
     
+        return self.df
