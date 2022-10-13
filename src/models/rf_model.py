feb94c2144dbd07fda80f5926576bb9a5d712af6
HJ <hanjun@MacBookAir.local>
2022-10-13 17:51:44 +0900

    modeling

--- src/models/rf_model.py
+++ src/models/rf_model.py
@@ -1,5 +1,4 @@
 from tkinter import Grid
-import scipy
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import RandomizedSearchCV
 from sklearn.metrics import f1_score
@@ -11,6 +10,8 @@ import numpy as np
 import seaborn as sns
 import eli5
 from eli5.sklearn import PermutationImportance
+from sklearn.inspection import permutation_importance
+import joblib
 
 
 class RF():
@@ -20,9 +21,9 @@ class RF():
         self.train_Y = train_Y
         self.val_X = val_X
         self.val_Y = val_Y
-        self.params = {'n_estimators': list(np.linspace(100, 5000, 50)),
-                         'max_depth': list(range(3,11)),
-                         'min_samples_leaf': [5,10,15,20]}
+        self.params = {'n_estimators': [2000, 3000, 4000],
+                         'max_depth': [5, 7 ,9],
+                         'min_samples_leaf': [10, 15, 20]}
         self.random_state = random_state
         
         
@@ -32,24 +33,34 @@ class RF():
         params = self.params
         
         rf_model = RandomForestClassifier()
-        rf_grid = RandomizedSearchCV(rf_model, param_distributions= params, n_iter=5, scoring="f1", cv=5, refit=True, random_state=self.random_state)
+        rf_grid = RandomizedSearchCV(rf_model, param_distributions= params, n_iter=3, scoring="f1", cv=3, refit=True, random_state=self.random_state)
         rf_grid.fit(train_X, train_Y)
         
         print('best parameters : ', rf_grid.best_params_)
         print('best val score : ', round(rf_grid.best_score_, 4))
         
         best_model = rf_grid.best_estimator_
+        joblib.dump(best_model, "models/saved_model/rf_best.pkl")
         
         return best_model
+    
+    def model_train(self):
+        print('✅ model_train')
+        rf_model = RandomForestClassifier(n_estimators=2000, max_depth=7, min_samples_leaf=10, random_state=self.random_state)
+        rf_model.fit(self.train_X, self.train_Y)
+        
+        # joblib.dump(rf_model, "models/saved_model/rf_model.pkl")
+        return rf_model
     
     def val_score(self, model):
         val_X = self.val_X
         val_Y = self.val_Y
         
         pred = model.predict(val_X)
-        F1_score = f1_score(val_Y, pred)
+        F1_score = f1_score(val_Y, pred, average='macro', labels=np.unique(pred))
         print("f1_score: ", F1_score)
         print(classification_report(val_Y, pred))
+        return F1_score
         
     def confusion_matrix(self, model):
         val_X = self.val_X
@@ -65,17 +76,20 @@ class RF():
         plt.show()
     
     def feature_importance(self, model):
+        print('✅ feature_importance')
         ftr_importances_values = model.feature_importances_
-        ftr_importances = pd.Series(ftr_importances_values, index = self.test_X.columns)
-        ftr_importances = ftr_importances.sort_values(ascending=False)
+        ftr_importances = pd.Series(ftr_importances_values, index = self.train_X.columns)
+        ftr_importances_20 = ftr_importances.sort_values(ascending=False)[:20]
         
         plt.figure(figsize=(8,6))
         plt.title('Feature Importances')
-        sns.barplot(x=ftr_importances, y=ftr_importances.index)
+        sns.barplot(x=ftr_importances_20, y=ftr_importances_20.index)
         plt.show()
+        return ftr_importances_20, ftr_importances_20.index
         
         
     def shap(self, model):
+        print('✅ shap')
         val_X = self.val_X
         
         explainer = shap.TreeExplainer(model) 
@@ -85,11 +99,28 @@ class RF():
         plt.show()
     
     def permutation_importance(self, model):
+        print('✅ permutation_importance')
         val_X = self.val_X
         val_Y = self.val_Y
-        perm = PermutationImportance(model, scoring = "f1", random_state = self.random_state).fit(val_X, val_Y)
-        eli5.show_weights(perm, top = 20, feature_names = val_X.columns.tolist())
+       
+        perm_imp = PermutationImportance(model, scoring='f1')
+        perm_imp.fit(val_X, val_Y)
+        # 순열 중요도 데이터 프레임 생성
+        perm_imp_df = pd.DataFrame()
+        feature = val_X.columns
+        perm_imp_df["feature"] = feature
+        perm_imp_df["importance"] = perm_imp.feature_importances_
+        perm_imp_df.sort_values(by='importance', ascending=False, inplace=True)
+        perm_imp_df.reset_index(drop=True, inplace=True)
+        perm_imp_df
         
+        plt.figure(figsize=(10,8))
+
+        sns.barplot(x='importance', y='feature', data=perm_imp_df)
+        plt.title('permutation importance', fontsize=18)
+
+        plt.show()
+    
     def pred_testset(self, test_X, model):
         pred = model.predict(test_X)
         test_X['is_applied'] = pred
--- src/models/xgb_model.py
+++ src/models/xgb_model.py
@@ -5,10 +5,13 @@ from sklearn.metrics import f1_score
 from sklearn.metrics import confusion_matrix, plot_confusion_matrix, classification_report
 import matplotlib.pyplot as plt
 import shap
+import pandas as pd
 import numpy as np
 import seaborn as sns
 import eli5
 from eli5.sklearn import PermutationImportance
+from sklearn.inspection import permutation_importance
+import joblib
 
 
 class XGB():
@@ -31,15 +34,15 @@ class XGB():
         val_Y = self.val_Y
         params = self.params
         
-        def evaluate_macroF1_lgb(y_true, y_pred):  
-            y_pred_label = np.round(y_pred)
-            f1 = f1_score(y_true, y_pred_label, average='macro')
-            print('f1 score:', f1)
-            return ('f1_score', f1, True)
+        # def evaluate_macroF1_lgb(y_true, y_pred):  
+        #     y_pred_label = np.round(y_pred)
+        #     f1 = f1_score(y_true, y_pred_label, average='macro')
+        #     print('f1 score:', f1)
+        #     return ('f1_score', f1, True)
         
         xgb = XGBClassifier()
-        xgb_grid = RandomizedSearchCV(xgb, param_distributions= params, n_iter=3, scoring="f1", cv=3, refit=True, random_state=self.random_state)
-        xgb_grid.fit(train_X, train_Y, early_stopping_rounds=50, eval_metric=evaluate_macroF1_lgb, eval_set=[(val_X, val_Y)])
+        xgb_grid = RandomizedSearchCV(xgb, param_distributions= params, n_iter=3, scoring="f1", cv=3, refit=True,verbose=0, random_state=self.random_state)
+        xgb_grid.fit(train_X, train_Y, early_stopping_rounds=50, eval_metric='auc', eval_set=[(val_X, val_Y)],verbose=False)
         
         print('best parameters : ', xgb_grid.best_params_)
         print('best val score : ', round(xgb_grid.best_score_, 4))
@@ -48,7 +51,16 @@ class XGB():
         
         return best_model
     
+    def model_train(self):
+        print('✅ model_train')
+        XGB_model = XGBClassifier(n_estimators=4000, max_depth=9, learning_rate=0.1, random_state=self.random_state)
+        XGB_model.fit(self.train_X, self.train_Y)
+        
+        # joblib.dump(XGB_model, "models/saved_model/rf_model.pkl")
+        return XGB_model
+    
     def val_score(self, model):
+        print('✅ val_score')
         val_X = self.val_X
         val_Y = self.val_Y
         
@@ -71,16 +83,18 @@ class XGB():
         plt.show()
         
     def feature_importance(self, model):
+        print('✅ feature_importance')
         fig, ax = plt.subplots(figsize=(9,11))
         plot_importance(model, ax=ax)
         plt.show()
         
         
     def shap(self, model):
+        print('✅ shap')
         val_X = self.val_X
         
         explainer = shap.TreeExplainer(model) 
-        shap_values = explainer.shap_values(val_X) 
+        shap_values = explainer.shap_values(val_X)
         
         plt.subplot(121)
         shap.initjs() 
@@ -92,10 +106,27 @@ class XGB():
         plt.show()
         
     def permutation_importance(self, model):
+        print('✅ permutation_importance')
         val_X = self.val_X
         val_Y = self.val_Y
-        perm = PermutationImportance(model, scoring = "f1", random_state = self.random_state).fit(val_X, val_Y)
-        eli5.show_weights(perm, top = 20, feature_names = val_X.columns.tolist())
+       
+        perm_imp = PermutationImportance(model, scoring='f1')
+        perm_imp.fit(val_X, val_Y)
+        # 순열 중요도 데이터 프레임 생성
+        perm_imp_df = pd.DataFrame()
+        feature = val_X.columns
+        perm_imp_df["feature"] = feature
+        perm_imp_df["importance"] = perm_imp.feature_importances_
+        perm_imp_df.sort_values(by='importance', ascending=False, inplace=True)
+        perm_imp_df.reset_index(drop=True, inplace=True)
+        perm_imp_df
+        
+        plt.figure(figsize=(10,8))
+
+        sns.barplot(x='importance', y='feature', data=perm_imp_df)
+        plt.title('permutation importance', fontsize=18)
+
+        plt.show()
         
     def pred_testset(self, test_X, model):
         pred = model.predict(test_X)
